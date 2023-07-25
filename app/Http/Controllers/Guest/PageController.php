62d5b458fdf1e2caee9695c12437da015e3eaad5
niccoloruberti <nikiruberti@gmail.com>
2023-07-25 18:47:40 +0200

    esercizio base completato

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Comic;
+
+class PageController extends Controller
+{
+    public function index() {
+
+        $comics = Comic::all();
+
+        return view('home', compact('comics'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+
+<main>
+    <div class="container">
+        <div class="row">
+            @foreach($comics as $comic)
+            <div class="col-3 my-3">
+                <div class="content h-100">
+                    <div class="card h-100">
+                        <img src="{{$comic['thumb']}}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$comic['title']}}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</main>
+
+</body>
+
+</html>
